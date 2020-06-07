680efd2cfbf5279eb07ad6aa3b3b0705924e47aa
adriaauca <adriaac99@gmail.com>
2020-06-07 18:06:00 +0200

    props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function HelloWorld(props) {
+  return (
+  <div id="hello">
+    <h3>{props.subttitle}</h3>
+    {props.mytext}
+  </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component:
+      <HelloWorld mytext="Hello 1" subttitle="Answer 1:" />
+      <HelloWorld mytext="Hello 2" subttitle="Answer 2:" />
+      <HelloWorld mytext="Hello 3" subttitle="Answer 3:" />
     </div>
   );
 }
